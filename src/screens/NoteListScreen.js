6adb4e7d4c0f946665851fd67c6ce86366c02b51
Sakib all hasan <sakibul15.3883@gmail.com>
2023-08-10 17:19:20 +0600

    copy content  & delete feature added

--- src/screens/NoteListScreen.js
+++ src/screens/NoteListScreen.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert,Clipboard} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native'; 
 import LinearGradient from 'react-native-linear-gradient';
 
 const gradientColors = ['#4e54c8', '#8f94fb'];
 
 const NoteListScreen = ({ navigation }) => {
   const [notes, setNotes] = useState([]);
-
+  // Load notes from AsyncStorage when the component mounts
   useEffect(() => {
     // Load notes from AsyncStorage when the component mounts
     loadNotes();
   }, []);
+ // Reload notes when the screen gains focus
+  useFocusEffect(() => {
+    loadNotes();
+  });
 
   const loadNotes = async () => {
     try {
@@ -25,8 +30,68 @@ const NoteListScreen = ({ navigation }) => {
     }
   };
 
+  const deleteNote = async (noteId) => {
+    try {
+      // Show confirmation alert before deleting the note
+      Alert.alert(
+        'Delete Note',
+        'Are you sure you want to delete this note?',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'Delete',
+            onPress: async () => {
+              const updatedNotes = notes.filter((note) => note.id !== noteId);
+              setNotes(updatedNotes);
+              await AsyncStorage.setItem('notes', JSON.stringify(updatedNotes));
+            },
+            style: 'destructive',
+          },
+        ],
+        { cancelable: false }
+      );
+    } catch (error) {
+      console.error('Error deleting note:', error);
+    }
+  };
+
+  const copyToClipboard = (text) => {
+    Clipboard.setString(text);
+    Alert.alert('Copied to Clipboard', `The text "${text}" has been copied to the clipboard.`);
+  };
+
   const renderNoteItem = ({ item }) => (
-    <TouchableOpacity style={styles.noteItem} onPress={() => navigation.navigate('AddNote', { note: item })}>
+    <TouchableOpacity
+      style={styles.noteItem}
+      onPress={() => navigation.navigate('AddNote', { note: item })}
+      onLongPress={() => {
+        Alert.alert(
+          'Note Actions',
+          'What do you want to do?',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+              text: 'Delete',
+              onPress: () => deleteNote(item.id),
+              style: 'destructive',
+            },
+            
+            {
+              text: 'Copy Content',
+              onPress: () => copyToClipboard(item.content),
+            },
+            
+          ],
+          { cancelable: true }
+        );
+      }} // Long press for actions: copy title, copy content, or delete
+    >
       <Text style={styles.noteTitle}>{item.title}</Text>
       <Text>{item.content}</Text>
     </TouchableOpacity>
@@ -34,18 +99,16 @@ const NoteListScreen = ({ navigation }) => {
 
   return (
     <LinearGradient colors={gradientColors} style={styles.gradient}>
-    <View style={styles.container}>
-      
-      <FlatList
-        data={notes}
-        renderItem={renderNoteItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-      <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('AddNote')}>
-        <Text style={styles.addButtonText}>Add Note</Text>
-      </TouchableOpacity>
-     
-    </View>
+      <View style={styles.container}>
+        <FlatList
+          data={notes}
+          renderItem={renderNoteItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('AddNote')}>
+          <Text style={styles.addButtonText}>Add Note</Text>
+        </TouchableOpacity>
+      </View>
     </LinearGradient>
   );
 };
@@ -55,7 +118,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
-
   gradient: {
     flex: 1,
     alignItems: 'center',
